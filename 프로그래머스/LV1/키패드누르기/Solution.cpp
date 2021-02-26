7649277fce84d63f5b30be6efd9402c729bb520b
inuit57 <inuit57@naver.com>
2021-02-26 11:39:42 +0900

    Update Solution.cpp

--- 프로그래머스/LV1/키패드누르기/Solution.cpp
+++ 프로그래머스/LV1/키패드누르기/Solution.cpp
@@ -31,65 +31,11 @@ string solution(vector<int> numbers, string hand) {
                 answer += 'R' ; 
                 right = numbers[i] ; 
             }
-            else{ // 2,5,8,0 일 때... 
-                // 1) 같은 위치인 경우 : 위에서 처리
-                // 2) 둘다 가운데 줄이 아닌 경우 (거리 체크)
-                // 2-1) 둘 중 하나가 가운데 줄인 경우 (거리 체크)
-                // 4) 같은 거리일 경우의 처리
+            else{ // 2,5,8,11 일 때... 
                 int l_chk , r_chk; 
-                l_chk = r_chk = 999; //혹시 모르니까 초기화. 
-    
-                //if( numbers[i] == 0 ) numbers[i] = 11; 
+                l_chk = r_chk = 0; //혹시 모르니까 초기화. 
+                //이거랑 뭔가 관련이 있을 수도 있겠는데 .
                 
-                /*
-                //left 처리
-                if(left > numbers[i]){
-                    l_chk = left - numbers[i] ; 
-                }else{
-                    l_chk = numbers[i] - left; 
-                }
-                
-                
-                if (l_chk %3 ==0 ) {
-                    l_chk /=3 ; 
-                }else{
-                    l_chk %= 3; 
-                }
-                
-                // 2일 때는 ....  1, 3 일 때 차이가 1 
-                // 2일 때 5라면 차이가 3.  (거리는 1)
-                // 2일 때 8이라면 차이가 6. (거리는 2)
-                // 2일 때는 /3 . %3하는게 맞다. (꼭 그런 것도 아닌 듯 싶다... )
-                // 
-                
-                
-                
-                // 5 일때?
-                // 4,6이라면 차이가 1 (거리는 1) %3
-                // 2,8이라면 차이가 3 (거리는 1) /3
-                // 
-                // 10,12 라면 차이가 5, 7 (거리는 3)... %3 하면 2로 나온다. 
-                
-                // 1,3이라면 차이가 2 (거리는 2) %3해도 괜찮다
-                // 7, 9 이라면 차이가 4.(거리는 2) %3하면 안된다. 
-                // 11(0)이라면 차이가 6 (거리는 3) %3하면 안된다. 
-                // 나누기 2를 하는 것도 좀 그렇네 근데. 
-                
-                //right 처리
-                if( right > numbers[i] ){
-                    r_chk = right - numbers[i] ; 
-                }else{
-                    r_chk = numbers[i] - right; 
-                }
-                
-                
-                if (r_chk %3 ==0 ) {
-                    r_chk /=3 ; 
-                }else{
-                    r_chk %= 3; 
-                }
-                */
-               
                 switch(numbers[i]){
                     case 2: 
                         // 거리 1 : L(1), M(5), R(3) 
@@ -123,19 +69,6 @@ string solution(vector<int> numbers, string hand) {
                             case 12 :
                                 r_chk = 4; break;
                         }
-                        
-                        //left 계산.
-                        // if(left%3 != 2){
-                        //     l_chk = left/3  +1 ; 
-                        // }else{
-                        //     l_chk = left/3 - 1 ; 
-                        // }
-                        // //right 계산. 
-                        // if(right % 3 != 2){
-                        //     r_chk = right/ 3; 
-                        // }else{
-                        //     r_chk = right/3 -1 ; 
-                        // }
                         break; // 따로 여기에서 작업칠 거는 없다.                       
                         
                     case 5:
@@ -179,9 +112,6 @@ string solution(vector<int> numbers, string hand) {
                         //거리 2 : L(4),L(10), M(2), R(6),R(12) 
                         //거리 3 : L(1), R(3)     
                        
-                        // printf("left :%d\n",left); 
-                        // printf("right :%d\n", right);
-                       
                         switch(left){
                             case 7:
                             case 5:
@@ -214,16 +144,13 @@ string solution(vector<int> numbers, string hand) {
                         }
                         break;
                     case 11: // 0 일 때. 
-                        //거리 1 : L(11) ,M(8),  R(12) 
+                        //거리 1 : L(10) ,M(8),  R(12) 
                         //거리 2 : L(7), M(5), R(9) 
                         //거리 3 : L(4), M(2), R(6) 
                         //거리 4 : L(1) , R(3) 
-
-//                         printf("left :%d\n",left); 
-//                         printf("right :%d\n", right);
-                        
+                  
                         switch(left){
-                            case 11:
+                            case 10: //여기가 11로 잘못 적혀있었다.
                             case 8:
                                 l_chk = 1;break;
                             case 7:
@@ -252,6 +179,9 @@ string solution(vector<int> numbers, string hand) {
                         break;
                 } // 케이스 문 끝. 
                 
+//                 printf("now : %d \n", numbers[i]); 
+//                 printf("l_chk : %d \n", l_chk); 
+//                 printf("r_chk : %d \n", r_chk); 
                 
                 
                 if(l_chk == r_chk){
