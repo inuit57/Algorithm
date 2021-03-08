8392cb038120cc7aa10d0d36d1bd46a06354c2cf
inuit57 <inuit57@naver.com>
2021-03-08 15:03:51 +0900

    Update and rename 모의고사(수정중).cpp to 모의고사.cpp
    순위 정렬하는 부분에서 헷갈렸던 모양이다. 
    
    다음부터는 저렇게 그냥 max 를 뽑아주고 max와 동일한 놈을 찾아서 넣어주는 식으로 하는 편이 좀더 깔끔하리라고 여겨진다.

--- 프로그래머스/LV1/완전탐색/모의고사.cpp
+++ 프로그래머스/LV1/완전탐색/모의고사.cpp
@@ -1,5 +1,6 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42840
 // signal: illegal instruction (core dumped) 에러가 b, c 를 건드리려고 하면 난다. 
+// 위의 에러는 answers, answer를 혼용해서 잘못 사용해서 난 에러다. 
 #include <string>
 #include <vector>
 
@@ -8,9 +9,7 @@ using namespace std;
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
     
-    int a = 0 ; 
-    int b = 0 ; 
-    int c = 0 ; 
+    int arr[3] = {0}; 
     
     int tmp = 0 ; 
     
@@ -32,83 +31,64 @@ vector<int> solution(vector<int> answers) {
     // 3 3 1 1 2 2 4 4 5 5 | 3 
     // 10으로 나누면 될 것으로 보여진다. 
     
-    switch(answers[0]){
-        case 1: a++; break;
-        case 2: b++; break;
-        case 3: c++; break; 
-    }
-    
-    for(int i= 1 ; i< answers.size() ; i++){
+    for(int i= 0 ; i< answers.size() ; i++){
         
         if(answers[i] == ((i%5)+1) ){
-            a++; 
+            arr[0]++; 
         }
         
         if(i%2 ==0){
             if (answers[i] == 2){
-                b++;
+                arr[1]++;
             }
         }else{
            if(i%8 == 1){
-               if(answers[i] == 1) b++; 
+               if(answers[i] == 1) arr[1]++; 
            }else if(i%8 == 3){
-               if(answers[i] == 3) b++;
+               if(answers[i] == 3) arr[1]++;
                
            }else if(i%8 == 5){
-               if(answers[i] == 4) b++; 
+               if(answers[i] == 4) arr[1]++; 
            }else if(i%8 == 7){
-               if(answers[i] == 5) b++; 
+               if(answers[i] == 5) arr[1]++; 
            }        
         }// b 끝
         
         tmp = i %10 ;
         if( tmp < 2){
-             if(answers[i] == 3) c++; 
+             if(answers[i] == 3) arr[2]++; 
         }else if(tmp < 4){
-                if(answers[i] == 1) c++; 
+                if(answers[i] == 1) arr[2]++; 
         }else if (tmp <6 ){
-                if(answers[i] == 2) c++; 
+                if(answers[i] == 2) arr[2]++; 
         }else if(tmp <8){
-                if(answers[i] == 4) c++; 
+                if(answers[i] == 4) arr[2]++; 
         }else{
-                if(answers[i] == 5) c++; break;
+                if(answers[i] == 5) arr[2]++; 
         }
 
     } // for문 끝.
 
+ //   printf("%d %d %d\n", a,b,c);
     
-    // 3항 연산자로 굴려야하나. 
-    // 다 같은 경우의 처리가..
-    if ( a > b){
-        if(a > c){
-            answer.push_back(1); 
-        }else{ // a<= c
-            if ( a == c){
-                answer.push_back(1); 
-            }
-            answer.push_back(3); 
+    
+//     answer.push_back(a); 
+//     answer.push_back(b); 
+//     answer.push_back(c); 
+    int max = -1 ; 
+    int who = -1 ; 
+    int cnt = 0 ; 
+    for(int i = 0 ; i< 3; i++){
+        if ( arr[i] >= max){
+            max = arr[i] ; 
+            who = i ; 
         }
-    }else if(b > a){ // 
-        if(b > c){
-            answer.push_back(2); 
-        }else{ // b <= c
-            if(b == c){
-                answer.push_back(2);     
-            }
-            answer.push_back(3); 
+    }
+ 
+    for(int i = 0 ; i< 3; i++){
+        if ( arr[i] == max){
+            answer.push_back(i+1);  
         }
-    }else{  // a == b
-        if ( a > c){
-            answer.push_back(1); 
-            answer.push_back(2); 
-        }else if (a < c){
-            answer.push_back(3); 
-        }else{
-            answer.push_back(1); 
-            answer.push_back(2); 
-            answer.push_back(3); 
-        }
-        
     }
     
     return answer;
