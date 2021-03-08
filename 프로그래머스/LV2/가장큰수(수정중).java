aad1813022322c5e10d3f1dc7ca6ceddb2431bc0
inuit57 <inuit57@naver.com>
2021-03-08 17:01:09 +0900

    Update 가장큰수(수정중).java

--- 프로그래머스/LV2/가장큰수(수정중).java
+++ 프로그래머스/LV2/가장큰수(수정중).java
@@ -1,56 +1,42 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42746?language=java                  
+//정렬함수를 만들어서 처리해야 한다. 
+// 맨앞의 숫자부터 비교해서 큰 순서대로 정렬해야 한다. 
+// 1) 첫자리를 비교해서 큰 놈이 앞으로 온다. 
+// 2) 첫자리가 같은 경우 다음 자리를 비교해서 큰 놈이 앞으로 온다. 
+// 3) 차이가 날 때까지 반복한다. 
+// 4) 만약 다음 자리가 없다면 없는 놈을 앞으로 놓는다. 
+
+// 9999 vs 90 을 생각해본다. 9999가 먼저 오는게 맞다. 
+// 9 vs 9999 를 생각해본다. 상관없다. 
+// 5 vs 9999 를 생각해본다. 9999가 앞으로 와야 한다. 
+// 999 vs 9998 을 생각해본다. 999가 앞으로 와야 한다. 
+// 30 vs 3 : 3이 앞으로 와야 한다. 
+    
+
+// 일반적인 문자열 정렬로 처리하는 것도 가능할 것 같다. 
+// charAt() 을 사용하면 0 ~9 까지 처리가 된다. 
+// 그냥 sort()를 돌리면 반대로 되길래 reverse로 방향 바꿔서 해봤더니 
+//  30 vs 3 같은 것을 30을 더 앞으로 놓아버린다. 
+// 이를 고치려면 커스텀 sort 함수를 구현해야 할텐데 그걸 좀 고민해봐야 할듯. 
+
 import java.util.* ; 
 
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-                   
-    //정렬함수를 만들어서 처리해야 한다. 
-    // 맨앞의 숫자부터 비교해서 큰 순서대로 정렬해야 한다. 
-    // 1) 첫자리를 비교해서 큰 놈이 앞으로 온다. 
-    // 2) 첫자리가 같은 경우 다음 자리를 비교해서 큰 놈이 앞으로 온다. 
-    // 3) 차이가 날 때까지 반복한다. 
-    // 4) 만약 다음 자리가 없다면 없는 놈을 앞으로 놓는다. 
-    
-    // 9999 vs 90 을 생각해본다. 9999가 먼저 오는게 맞다. 
-    // 9 vs 9999 를 생각해본다. 상관없다. 
-    // 5 vs 9999 를 생각해본다. 9999가 앞으로 와야 한다. 
-    // 999 vs 9998 을 생각해본다. 999가 앞으로 와야 한다. 
-    
-        Collections.sort(numbers , new Comparator<int[]>() { //이렇게 하니까 에러가 난다. 
-          //그냥 통째로 String 배열에 때려박아버리고 처리하는 걸 생각해보자. 
-            @Override
-            public int compare(int a, int b) {
-                String as , bs ; 
-                as = bs = ""; 
-                
-                //문자열로 바꾸고 문자열에서 한글자씩 따서 처리
-                as +=a; 
-                bs +=b; 
-                
-                if(as.length() >= bs.length){
-                    for(int i = 0 ; i < bs.length ; i++){
-                        if(as.charAt(i) > bs.charAt(i)){
-                            return 1; 
-                        }else if(as.charAt(i) < bs.charAt(i)){
-                            return -1 ; 
-                        }
-                    }
-                    return -1 ; 
-                }else{
-                    for(int i = 0 ; i < as.length ; i++){
-                        if(as.charAt(i) > bs.charAt(i)){
-                            return 1; 
-                        }else if(as.charAt(i) < bs.charAt(i)){
-                            return -1 ; 
-                        }
-                    }
-                    return 1 ;
-                }
-                
-            }
-} );
-  
-     System.out.println("%d", numbers[i]);        
+        String[] arr ; 
+   
+        arr = new String[numbers.length]; 
+        for(int i = 0 ; i< numbers.length ; i++){
+            arr[i] = ""+numbers[i] ; 
+        }   
+       
+        Arrays.sort(arr , Collections.reverseOrder());  // 여기에 커스텀 sort 함수 넣기. 
+        
+        for(int i = 0 ; i< arr.length ; i++){
+            answer += arr[i] ; 
+        }
+        
         return answer;
     }
 }
