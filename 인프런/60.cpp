2f12cf059d0a6a5b24c1cb92596bb8f68b872c61
inuit57 <inuit57@naver.com>
2021-04-01 17:40:17 +0900

    Create 60.cpp

---
+++ 인프런/60.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+#include<algorithm>
+
+using namespace std; 
+
+int n  ; 
+int check[10] = {0} ;
+int arr[10] ;  
+
+int MAX ; 
+void d(int num , long sum){
+
+    if ( num >= n ){
+        //print.. 
+        int sum[2] = {0} ; 
+        for(int i =0 ; i< n ; i++){
+            sum[check[i]] += arr[i] ; 
+        }
+
+        if(sum[0] == sum[1]){
+            //printf("YES\n"); 
+            //printf("%d\n", sum[0]);
+            if ( sum[0] > MAX) {
+                MAX = sum[0] ; 
+            } 
+        }
+        // //printf("\n");
+        // printf("%d\n", sum) ; 
+        // v_sum.push_back(sum) ; 
+        return ;  
+    } else{
+        check[num] = 1 ; 
+        d(num+1 , sum + arr[num]) ; 
+        check[num] = 0 ; 
+        d(num+1 , sum); 
+    }
+
+}
+int main(){
+
+    MAX = -1; 
+    cin >> n ; 
+    for(int i = 0 ; i< n; i++){
+        cin >> arr[i] ; 
+    }
+
+    //long sum = 0 ; 
+    d( 0  , 0 ); 
+    
+    if (MAX > 0 ) {
+        printf("YES"); 
+//        return 0; 
+    }else{
+        printf("NO"); 
+    }
+
+    return 0; 
+
+}
+
+//합이 같은 부분집합.
+//2개의 집합으로 나누었을 때 합이 같은 경우 YES를 출력.
+
+// 생각보다 간단하게 풀렸다. 
+// 0 , 1로 체크해서 2개의 집합으로 나누는 것이 풀이의 핵심 
